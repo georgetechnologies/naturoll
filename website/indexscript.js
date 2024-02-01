de8bdadf3ad7b3ba0b4700f11630d36938227686
georgetechnologies <157044536+georgetechnologies@users.noreply.github.com>
2024-02-01 12:16:58 +0100

    Update indexscript.js
    working with index.html !!

--- website/indexscript.js
+++ website/indexscript.js
@@ -1,9 +1,11 @@
-let button = document.querySelector("#boutonDiscover")
-let errorMsg = document.createElement("p")
+let bouton = document.querySelector("button")
+let errorMsg = document.createElement("h2")
 let errorMsgLocation = document.querySelector("#errorMsg")
 
-button.addEventListener("click", function onClick () {
-    window.location("naturoll.html")
+console.log(bouton)
+
+bouton.addEventListener("click", function onClick () {
+    window.location = "naturoll.html"
     errorMsg = `La page charge, soyez patient :) !`
     errorMsgLocation.prepend(errorMsg)
 })
