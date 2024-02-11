8e61f71fe77035549c8bb8654decaf0dece690a2
nympp <159667313+nympp@users.noreply.github.com>
2024-02-11 04:34:04 +0100

    Update globalscript.js

--- website/globalscript.js
+++ website/globalscript.js
@@ -38,26 +38,26 @@ footerContent.innerHTML = `
             Retrouvez George Technologies / Naturoll sur :
             <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/4/4f/Discord_Logo_sans_texte.svg/1818px-Discord_Logo_sans_texte.svg.png" class="footerlinks" id="LienDiscord">
             <img src="https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png" class="footerlinks" id="LienX" style="background-color: black; border-radius: 5px;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png" class="footerlinks" id="LienInstagram">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" class="footerlinks" id="LienGithub">
         </center>
     </footer>
 `
 footerLocation.append(footerContent)
 
 let discordClick = document.querySelector("#LienDiscord")
-let XClick = document.querySelector("#LienX")
-let instagramClick = document.querySelector("#LienInstagram")
+let xClick = document.querySelector("#LienX")
+let githubClick = document.querySelector("#LienGithub")
 
 console.log(discordClick)
-console.log(XClick)
-console.log(instagramClick)
+console.log(xClick)
+console.log(githubClick)
 
 discordClick.addEventListener("click", function onClick () {
     window.location = "https://www.discord.gg/naturollnbx"
 })
-XClick.addEventListener("click", function onClick () {
+xClick.addEventListener("click", function onClick () {
     window.location = "https://www.x.com/NaturollNBX"
 })
-instagramClick.addEventListener("click", function onClick () {
-    window.location = "https://www.instagram.com/NaturollNBX"
+githubClick.addEventListener("click", function onClick () {
+    window.location = "https://github.com/georgetechnologies/naturoll"
 })
