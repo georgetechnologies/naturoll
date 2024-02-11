3a563de3489f41f2f69eba125bf87cdc0d73ac56
nympp <159667313+nympp@users.noreply.github.com>
2024-02-11 05:12:11 +0100

    Add files via upload

---
+++ website/naturollcscript.js
@@ -0,0 +1,7 @@
+let buttonBackToNaturoll = document.querySelector("#backToNaturoll")
+
+console.log(buttonBackToNaturoll)
+
+buttonBackToNaturoll.addEventListener("click", function onClick () {
+    window.location = `naturoll.html`
+})
