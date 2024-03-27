5dd9280246b2e226087bb71f2b29869ad0cfc14c
nympp <159667313+nympp@users.noreply.github.com>
2024-03-27 21:24:57 +0100

    Update globalscript.js

--- website/globalscript.js
+++ website/globalscript.js
@@ -7,11 +7,10 @@ console.log(navContent)
 navContent.innerHTML = `
             <nav>
                 <ul>
-                   <li class="deroulant"><img src="Flag-France.png" width="25" height="17" alt="Drapeau Français" title="Drapeau Français"> Français (France) 
+                   <li class="deroulant">Langue 
                     <ul class="sous">
-                      <li><a href="index.html"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
-                      <li><a href="en/index.html"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
-                      <li><a href="#"><img src="Flag-People's-Republic-of-China.png" width="25" height="17" alt="Drapeau de la chine" title="Drapeau de la Chine"> 中文 (中国)</a></li>
+                      <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
+                      <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
                     </ul>
                   </li>
                   <li class="deroulant">Naviguer
@@ -27,32 +26,3 @@ navContent.innerHTML = `
             </nav>
 `
 navLocation.append(navContent)
-
-let footerLocation = document.querySelector("#footer")
-let footerContent = document.createElement("span")
-
-footerContent.innerHTML = `
-    <footer class="apple-ui">
-        <center><br><br><br><br>
-            Toutes les images utilisées sur le site internet du Naturoll sont libre de droits ou crées de toutes pièce par George Technologies. Certaines images on été crées par des Intelligences Artificielles. <a href="ia-use.html">En savoir plus.</a><br>
-            Site développé par <a href="phileas.html">Philéas</a> aidé par <a href="lucas.html">Lucas</a>.<br>
-            Retrouvez George Technologies / Naturoll sur :
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png" class="footerlinks" id="LienX" style="background-color: black; border-radius: 5px;">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" class="footerlinks" id="LienGithub">
-        </center>
-    </footer>
-`
-footerLocation.append(footerContent)
-
-let xClick = document.querySelector("#LienX")
-let githubClick = document.querySelector("#LienGithub")
-
-console.log(xClick)
-console.log(githubClick)
-
-xClick.addEventListener("click", function onClick () {
-    window.location = "https://www.x.com/NaturollNBX"
-})
-githubClick.addEventListener("click", function onClick () {
-    window.location = "https://github.com/georgetechnologies/naturoll"
-})
