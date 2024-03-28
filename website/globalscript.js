1c2221d4e6819f3c2cab71aef822cf386fc5366f
georgetechnologies <157044536+georgetechnologies@users.noreply.github.com>
2024-03-28 09:54:10 +0100

    Update globalscript.js

--- website/globalscript.js
+++ website/globalscript.js
@@ -1,13 +1,40 @@
 let navLocation = document.querySelector("#navlocation")
 let navContent = document.createElement("span")
+let params = new URLSearchParams(window.location.search)
+let lang = params.get("lang")
 
 console.log(navLocation)
 console.log(navContent)
 
-navContent.innerHTML = `
+if(
+            lang === "en"
+){
+          navContent.innerHTML = `
             <nav>
                 <ul>
-                   <li class="deroulant">Langue 
+                   <li class="deroulant">English (USA) 
+                    <ul class="sous">
+                      <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
+                      <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
+                    </ul>
+                  </li>
+                  <li class="deroulant">Naviguer
+                    <ul class="sous">
+                        <li><a href="index.html">Accueil</a></li>
+                        <li><a href="naturoll.html">Naturoll</a></li>
+                        <li><a href="membres.html">Membres</a></li>
+                        <li><a href="listofpages.html">Toutes les pages du site</a></li>
+                    </ul>
+                  </li>
+                  <img src="navlogo.png" class="navLogo">
+                </ul>
+            </nav>
+`  
+} else {
+            navContent.innerHTML = `
+            <nav>
+                <ul>
+                   <li class="deroulant">Français (France) 
                     <ul class="sous">
                       <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
                       <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
@@ -25,4 +52,5 @@ navContent.innerHTML = `
                 </ul>
             </nav>
 `
+}
 navLocation.append(navContent)
