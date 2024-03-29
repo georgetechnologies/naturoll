396d4de708084f35b40b29e0ab13f2cb4c674fcc
georgetechnologies <157044536+georgetechnologies@users.noreply.github.com>
2024-03-29 11:54:05 +0100

    Update lang.js

--- website/lang/naturoll.html/lang.js
+++ website/lang/naturoll.html/lang.js
@@ -30,11 +30,9 @@ async function main () {
     ){
         title.append(miseEnForme(text.forall[0].en))
         subtitle.append(miseEnForme(text.forall[1].en))
-        buttontext.append(miseEnForme(text.indexhtml[0].en))
     } else {
         title.append(miseEnForme(text.forall[0].fr))
         subtitle.append(miseEnForme(text.forall[1].fr))
-        buttontext.append(miseEnForme(text.indexhtml[0].fr))
     }
 }
 
