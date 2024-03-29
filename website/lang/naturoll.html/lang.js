711118e4b1005960154a3056d77420c0f4a9b82f
georgetechnologies <157044536+georgetechnologies@users.noreply.github.com>
2024-03-29 11:53:10 +0100

    Update lang.js

---
+++ website/lang/naturoll.html/lang.js
@@ -0,0 +1,43 @@
+let params = new URLSearchParams(window.location.search)
+let lang = params.get("lang")
+let title = document.querySelector("#TITLE")
+let subtitle = document.querySelector("#SUBTITLE")
+
+let titles1 = document.querySelector("#titles1")
+let titles2 = document.querySelector("#titles2")
+let titles3 = document.querySelector("#titles3")
+
+let text
+
+function miseEnForme (texts) {
+    let t = document.createElement("span")
+    t.innerHTML = `
+    <span>${texts}</span>
+    `
+    return t
+}
+
+async function main () {
+    let ans = await fetch("https://raw.githubusercontent.com/georgetechnologies/naturoll/main/website/lang/lang.json", {
+        headers: {
+            Accept: 'application/json'
+        }
+    })
+    text = await ans.json()
+
+    if(
+        lang === "en"
+    ){
+        title.append(miseEnForme(text.forall[0].en))
+        subtitle.append(miseEnForme(text.forall[1].en))
+        buttontext.append(miseEnForme(text.indexhtml[0].en))
+    } else {
+        title.append(miseEnForme(text.forall[0].fr))
+        subtitle.append(miseEnForme(text.forall[1].fr))
+        buttontext.append(miseEnForme(text.indexhtml[0].fr))
+    }
+}
+
+main()
+
+console.log(title)
