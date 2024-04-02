343f969a5c72e7d1aa103691c45dac59048b8fb5
nympp <159667313+nympp@users.noreply.github.com>
2024-04-02 19:52:58 +0200

    Update globalscript.js

--- website/globalscript.js
+++ website/globalscript.js
@@ -12,18 +12,18 @@ if(
           navContent.innerHTML = `
             <nav>
                 <ul>
-                   <li class="deroulant">English (USA) 
-                    <ul class="sous">
-                      <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
-                      <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
-                    </ul>
-                  </li>
                   <li class="deroulant">Browse
                     <ul class="sous">
                         <li><a href="index.html">Home</a></li>
                         <li><a href="naturoll.html">Naturoll</a></li>
                         <li><a href="membres.html">Members</a></li>
                         <li><a href="listofpages.html">Every pages of the website</a></li>
+                    </ul>
+                  </li>
+                  <li class="deroulant"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA">
+                    <ul class="sous">
+                      <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
+                      <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
                     </ul>
                   </li>
                   <img src="navlogo.png" class="navLogo">
@@ -34,18 +34,18 @@ if(
             navContent.innerHTML = `
             <nav>
                 <ul>
-                   <li class="deroulant">Français (France) 
-                    <ul class="sous">
-                      <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
-                      <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
-                    </ul>
-                  </li>
                   <li class="deroulant">Naviguer
                     <ul class="sous">
                         <li><a href="index.html">Accueil</a></li>
                         <li><a href="naturoll.html">Naturoll</a></li>
                         <li><a href="membres.html">Membres</a></li>
                         <li><a href="listofpages.html">Toutes les pages du site</a></li>
+                    </ul>
+                  </li>
+                  <li class="deroulant"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français">
+                    <ul class="sous">
+                      <li><a href="?lang=fr"><img src="Flag-France.png" width="25" height="17" alt="Drapeau de la republique Française" title="Drapeau Français"> Français (France)</a></li>
+                      <li><a href="?lang=en"><img src="Flag-United-States-of-the-America.png" width="25" height="17" alt="Drapeau des Etats-Unis D'Amérique" title="Drapeau des USA"> English (U.S.A.)</a></li>
                     </ul>
                   </li>
                   <img src="navlogo.png" class="navLogo">
