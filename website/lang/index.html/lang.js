315f81542d0298ca1357f7198a13f51877aa5463
nympp <159667313+nympp@users.noreply.github.com>
2024-04-03 23:48:57 +0200

    Update lang.js

--- website/lang/index.html/lang.js
+++ website/lang/index.html/lang.js
@@ -3,6 +3,9 @@ let lang = params.get("lang")
 let title = document.querySelector("#TITLE")
 let subtitle = document.querySelector("#SUBTITLE")
 let buttontext = document.querySelector("#discover")
+let Mbuttontext = document.querySelector("#Mdiscover")
+let Mtitle = document.querySelector("#MEMBERSTITLE")
+let Msubtitle = document.querySelector("#MEMBERSSUBTITLE")
 let text
 
 function miseEnForme (texts) {
@@ -25,12 +28,18 @@ async function main () {
         lang === "en"
     ){
         title.append(miseEnForme(text.indexhtml[1].en))
+        Mtitle.append(miseEnForme(text.indexhtml[2].en))
         subtitle.append(miseEnForme(text.forall[1].en))
+        Msubtitle.append(miseEnForme(text.indexhtml[3].en))
         buttontext.append(miseEnForme(text.indexhtml[0].en))
+        Mbuttontext.append(miseEnForme(text.indexhtml[0].en))
     } else {
         title.append(miseEnForme(text.indexhtml[1].fr))
+        Mtitle.append(miseEnForme(text.indexhtml[2].fr))
         subtitle.append(miseEnForme(text.forall[1].fr))
+        Msubtitle.append(miseEnForme(text.indexhtml[3].fr))
         buttontext.append(miseEnForme(text.indexhtml[0].fr))
+        Mbuttontext.append(miseEnForme(text.indexhtml[0].fr))
     }
 }
 
