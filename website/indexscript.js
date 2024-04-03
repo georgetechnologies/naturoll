2d171c23c7b6396a905b9f9406b8b2b302583f41
nympp <159667313+nympp@users.noreply.github.com>
2024-04-03 23:48:09 +0200

    Update indexscript.js

--- website/indexscript.js
+++ website/indexscript.js
@@ -1,11 +1,28 @@
-let bouton = document.querySelector("button")
-let errorMsg = document.createElement("h2")
-let errorMsgLocation = document.querySelector("#errorMsg")
+let bouton = document.querySelector("#discover")
+let mbouton = document.querySelector("#Mdiscover")
+let p = new URLSearchParams(window.location.search)
+let l = p.get("lang")
 
 console.log(bouton)
 
 bouton.addEventListener("click", function onClick () {
-    window.location = "naturoll.html"
-    errorMsg = `La page charge, soyez patient :) !`
-    errorMsgLocation.prepend(errorMsg)
+    if(
+        l === "en"
+    ){
+        window.location = "naturoll.html?lang=en"
+    }
+    else {
+        window.location = "naturoll.html?lang=fr"
+    }
+})
+
+mbouton.addEventListener("click", function onClick () {
+    if(
+        l === "en"
+    ){
+        window.location = "membres.html?lang=en"
+    }
+    else {
+        window.location = "membres.html?lang=fr"
+    }
 })
