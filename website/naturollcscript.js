cbe5a9098b11e66b032d6f5f77ab2b8851b63868
nympp <159667313+nympp@users.noreply.github.com>
2024-04-04 00:09:21 +0200

    Update naturollcscript.js

--- website/naturollcscript.js
+++ website/naturollcscript.js
@@ -1,7 +1,17 @@
 let buttonBackToNaturoll = document.querySelector("#backToNaturoll")
 
+let p = new URLSearchParams(window.location.search)
+let l = p.get("lang")
+
 console.log(buttonBackToNaturoll)
 
 buttonBackToNaturoll.addEventListener("click", function onClick () {
-    window.location = `naturoll.html`
+    if(
+        lang === "en"
+    ){
+        window.location = `naturoll.html?lang=en`
+    }
+    else {
+        window.location = `naturoll.html?lang=fr`
+    }
 })
