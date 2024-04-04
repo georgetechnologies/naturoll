3826a1571c743bef8a76d3322bfbe300921a3452
nympp <159667313+nympp@users.noreply.github.com>
2024-04-04 22:26:31 +0200

    Update lopscript.js

--- website/lopscript.js
+++ website/lopscript.js
@@ -2,16 +2,40 @@ let cat1 = document.querySelector("#accueilcat")
 let cat2 = document.querySelector("#naturollcat")
 let cat3 = document.querySelector("#membrescat")
 
+let p5 = new URLSearchParams(window.location.search)
+let l5 = p5.get("lang")
+
 console.log(cat1)
 console.log(cat2)
 console.log(cat3)
 
 cat1.addEventListener("click", function onClick () {
-    window.location = `index.html`
+    if(
+        l5 === "en"
+    ){
+        window.location = "index.html?lang=en"
+    }
+    else {
+        window.location = "index.html?lang=fr"
+    }
 })
 cat2.addEventListener("click", function onClick () {
-    window.location = `naturoll.html`
+    if(
+        l5 === "en"
+    ){
+        window.location = "naturoll.html?lang=en"
+    }
+    else {
+        window.location = "naturoll.html?lang=fr"
+    }
 })
 cat3.addEventListener("click", function onClick () {
-    window.location = `membres.html`
+    if(
+        l5 === "en"
+    ){
+        window.location = "membres.html?lang=en"
+    }
+    else {
+        window.location = "membres.html?lang=fr"
+    }
 })
