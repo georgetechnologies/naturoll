161974e14663307476f9a882fc40543eaac34bc8
nympp <159667313+nympp@users.noreply.github.com>
2024-04-04 06:59:45 +0200

    Update membrescscript.js

--- website/membrescscript.js
+++ website/membrescscript.js
@@ -1,9 +1,19 @@
 let back = document.querySelector("#backToMembersPage")
 
+let p = new URLSearchParams(window.location.search)
+let l = p.get("lang")
+
 console.log(back)
 
 back.addEventListener("click", function onClick () {
-    window.location = `membres.html`
+    if(
+        l === "en"
+    ){
+        window.location = "membres.html?lang=en"
+    }
+    else {
+        window.location = `membres.html?lang=fr`
+    }
 })
 
 let secretaire = document.querySelector("#secretaire")
@@ -37,7 +47,14 @@ secretaire.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
-    msgLocation.innerHTML = `Thomas est secrétaire. Le secrétaire remplit la feuille qui dit ce que fais chaque personne à chaque cours.`
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Thomas is the secretary. He fills out the paper the says what each personne does each classe.`
+    }
+    else {
+        msgLocation.innerHTML = `Thomas est secrétaire. Le secrétaire remplit la feuille qui dit ce que fais chaque personne à chaque cours.`
+    }
 })
 ingenieur.addEventListener("click", function onClick () {
     if(
@@ -45,7 +62,14 @@ ingenieur.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
-    msgLocation.innerHTML = `Thomas est ingénieur. L'ingénieur cherche les solutions techniques pour les différentes épreuves. Par exemple l'attrappe balle pour l'épreuve`
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Thomas is the engenier. THe engenier searcges for technical solutions for different tests. For example the balle catcher`
+    }
+    else {
+        msgLocation.innerHTML = `Thomas est ingénieur. L'ingénieur cherche les solutions techniques pour les différentes épreuves. Par exemple l'attrappe balle`
+    }
 })
 designer.addEventListener("click", function onClick () {
     if(
@@ -53,7 +77,14 @@ designer.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Joshua is the designer. The designer designes the robot's paintwork and the globales colors of the project.`
+    }
+    else {
     msgLocation.innerHTML = `Joshua est designer. Le designer designe la carosserie du robot et les couleurs globales du projet.`
+    }
 })
 gestionnaire.addEventListener("click", function onClick () {
     if(
@@ -61,7 +92,14 @@ gestionnaire.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Joshua is administrator. The admin manages the budget of the team and orders the necessary materiels.`
+    }
+    else {
     msgLocation.innerHTML = `Joshua est gestionnaire. Le gestionnaire gère le budget de l'équipe et passe les commandes de matériaux nécésaires.`
+    }
 })
 programmeur.addEventListener("click", function onClick () {
     if(
@@ -69,7 +107,14 @@ programmeur.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Lucas is the programmer. The programmer code the Naturoll robot with Microsoft Makecode for micro:bit wich lets him code using blocs like with MIT's scratch.`
+    }
+    else {
     msgLocation.innerHTML = `Lucas est programmeur. Le programmeur code le robot Naturoll avec Microsoft Makecode for micro:bit qui lui permet de coder en language bloc type scratch.`
+    }
 })
 technicien.addEventListener("click", function onClick () {
     if(
@@ -77,7 +122,14 @@ technicien.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Lucas is the technician. The Technician is in charge of all thing electronics on the robot.`
+    }
+    else {
     msgLocation.innerHTML = `Lucas est technicien. Le technicien s'occupe de la partie électronique du robot.`
+    }
 })
 traducteur.addEventListener("click", function onClick () {
     if(
@@ -85,7 +137,14 @@ traducteur.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Lucas is the translater. The translater translates the web pages on the website in english.`
+    }
+    else {
     msgLocation.innerHTML = `Lucas est traducteur. Le traducteur s'occupe de traduire les pages du site web en anglais.`
+    }
 })
 devadj.addEventListener("click", function onClick () {
     if(
@@ -93,7 +152,14 @@ devadj.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Lucas is the developper's assistant. The developper's assistant is helps the main developper when he needs help.`
+    }
+    else {
     msgLocation.innerHTML = `Lucas est développeur adjoint. Le développeur adjoint aide le développeur lors de difficultés rencontrées.`
+    }
 })
 chefDP.addEventListener("click", function onClick () {
     if(
@@ -101,7 +167,14 @@ chefDP.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Phileas is the project manager. It's him who manages the team.`
+    }
+    else {
     msgLocation.innerHTML = `Philéas est le chef de projet. C'est lui qui gère l'équipe en général.`
+    }
 })
 dev.addEventListener("click", function onClick () {
     if(
@@ -109,7 +182,14 @@ dev.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Phileas is the developper. He is in charge of developpes the site.`
+    }
+    else {
     msgLocation.innerHTML = `Philéas est développeur informatique. C'est lui qui est en charge du développement du site internet.`
+    }
 })
 progadj.addEventListener("click", function onClick () {
     if(
@@ -117,5 +197,12 @@ progadj.addEventListener("click", function onClick () {
     ){
         msgLocation.innerHTML = ``
     }
+    if(
+        l === "en"
+    ){
+        msgLocation.innerHTML = `Phileas is the programmer's assistant. He helps the programmers when he encounters a difficulty.`
+    }
+    else {
     msgLocation.innerHTML = `Philéas est programmeur adjoint. C'est lui qui aide le programmeur en cas de difficultés.`
+    }
 })
