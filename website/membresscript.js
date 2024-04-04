b54c2ff48063ed8672b84494cf1330067291efab
nympp <159667313+nympp@users.noreply.github.com>
2024-04-04 06:39:31 +0200

    Update membresscript.js

--- website/membresscript.js
+++ website/membresscript.js
@@ -3,20 +3,51 @@ let lucasLM = document.querySelector("#lucasbouton")
 let joshuaLM = document.querySelector("#joshuabouton")
 let phileasLM = document.querySelector("#phileasbouton")
 
+let p = new URLSearchParams(window.location.search)
+let l = p.get("lang")
+
 console.log(thomasLM)
 console.log(lucasLM)
 console.log(joshuaLM)
 console.log(phileasLM)
 
 thomasLM.addEventListener("click", function onClick () {
-    window.location = "thomas.html"
+    if(
+        l === "en"
+    ){
+        window.location = "thomas.html?lang=en"
+    }
+    else {
+        window.location = "thomas.html?lang=fr"
+    }
 })
 lucasLM.addEventListener("click", function onClick () {
-    window.location = "lucas.html"
+    if(
+        l === "en"
+    ){
+        window.location = "lucas.html?lang=en"
+    }
+    else {
+        window.location = "lucas.html?lang=fr"
+    }
 })
 joshuaLM.addEventListener("click", function onClick () {
-    window.location = "joshua.html"
+    if(
+        l === "en"
+    ){
+        window.location = "joshua.html?lang=en"
+    }
+    else {
+        window.location = "joshua.html?lang=fr"
+    }
 })
 phileasLM.addEventListener("click", function onClick () {
-    window.location = "phileas.html"
+    if(
+        l === "en"
+    ){
+        window.location = "phileas.html?lang=en"
+    }
+    else {
+        window.location = "phileas.html?lang=fr"
+    }
 })
