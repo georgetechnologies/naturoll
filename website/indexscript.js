e6fc58cb2fe0c15c04c3401a6e0972d8d01c3136
nympp <159667313+nympp@users.noreply.github.com>
2024-05-01 16:55:08 +0200

    Update indexscript.js

--- website/indexscript.js
+++ website/indexscript.js
@@ -1,5 +1,6 @@
 let bouton = document.querySelector("#db")
 let mbouton = document.querySelector("#mdb")
+let abouton = document.querySelector("#ADB")
 let p = new URLSearchParams(window.location.search)
 let l = p.get("lang")
 
@@ -24,5 +25,16 @@ mbouton.addEventListener("click", function onClick () {
     }
     else {
         window.location = "membres.html?lang=fr"
+    }
+})
+
+abouton.addEventListener("click", function onClick () {
+    if(
+        l === "en"
+    ){
+        window.location = "after.html?lang=en"
+    }
+    else {
+        window.location = "after.html?lang=fr"
     }
 })
